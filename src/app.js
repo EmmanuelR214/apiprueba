a82e8c9b996efb96911020ebca4d89d382ce53e4
EmmanuelR214 <127364880+EmmanuelR214@users.noreply.github.com>
2024-06-28 16:44:09 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 // }));
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://labarbada.store',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
