01732d6eecc828185706fd997c22b2ff64a82fc9
EmmanuelR214 <127364880+EmmanuelR214@users.noreply.github.com>
2024-06-28 17:54:21 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ const corsOptions = {
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true 
+}
 app.use(cors(corsOptions));
 
 
